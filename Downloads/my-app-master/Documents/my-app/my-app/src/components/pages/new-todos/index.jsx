0f3a505dcdc035dfeadf3706708311d3c3bb7701
Dana <dandonshak@gmail.com>
2021-10-15 11:55:55 -0400

    Added form for new todos

---
+++ Downloads/my-app-master/Documents/my-app/my-app/src/components/pages/new-todos/index.jsx
@@ -0,0 +1,9 @@
+import {NewTodoForm} from "../../new-todo-form"
+import "./styles.css"
+
+
+export const NewTodoPage = () => {
+    return (
+        <NewTodoForm></NewTodoForm>
+    )
+}
---
+++ Downloads/my-app-master/Documents/my-app/my-app/src/components/todo/index.jsx
@@ -0,0 +1,55 @@
+import PropTypes from "prop-types";
+
+import React, { useState, useEffect} from 'react';
+
+import { GoTrashcan, GoCheck} from 'react-icons/go';
+
+import './styles.css'
+
+export const Todo = (props) => {
+
+    const [isComplete, setIsComplete] = useState(false);
+
+    useEffect( () =>  {
+        setIsComplete(props.isComplete)
+    }, [props.isComplete])
+
+    const toggleCompleteTodo = () => {
+        setIsComplete (!isComplete);
+    }
+
+    return (
+        <div className={`todo ${props.color}`} style= { isComplete ? {opacity: 0.2} : {} }> 
+        
+        <div>
+            <p className="todo-text">{ props.text } </p>
+            <p className="todo-date">{ props.date } </p>
+        </div>
+
+        <div className="todo-btns">
+            <button className="todo-complete" onClick={toggleCompleteTodo} >
+                <GoCheck className="todo-icon" style={{ fontsize: "35px"}} />
+            </button>
+            <button className="todo-delete">
+                <GoTrashcan className="todo-icon" style={{ fontsize: "35px"}} />
+            </button>
+        </div>
+    </div>
+    )
+}
+
+Todo.propTypes = {
+    text: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    color: PropTypes.string,
+    priority: PropTypes.bool,
+    isComplete: PropTypes.bool
+}
+
+Todo.defaultProps = {
+    text: "",
+    date: "",
+    color: "gray",
+    priority: false,
+    isComplete: false
+}
